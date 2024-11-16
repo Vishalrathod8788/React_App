f1b09b9517a9ea0e28f10845c0f81d5715a9a790
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-11-16 22:43:50 +0530

    there are three way to code write

--- react_demo/src/App.jsx
+++ react_demo/src/App.jsx
@@ -1,8 +1,24 @@
 // import React from "react";
-import Fragment from "react";
+import { Fragment } from "react";
 
 export const App = () => {
   return (
+    // You Can't One more component return in JavaScript
+    // but You can Component value add in Array with Key and Pair to store and return
+    // but It's complicate so You will use to return <> write component code <> this is absulutly right
+
+    // Method 1 :
+    // <>
+    //   <h1>Welcome to My First React App!</h1>
+    //   <div className="card">
+    //     <p>Hello! I am learning React</p>
+    //     <button onClick={() => alert("You clicked the button!")}>
+    //       Click me!
+    //     </button>
+    //   </div>
+    // </>
+
+    // Method 2 :
     // <React.Fragment>
     //   <h1>Welcome to My First React App!</h1>
     //   <div className="card">
@@ -13,6 +29,7 @@ export const App = () => {
     //   </div>
     // </React.Fragment>
 
+    // Method 3 :
     <Fragment>
       <h1>Welcome to My First React App!</h1>
       <div className="card">
