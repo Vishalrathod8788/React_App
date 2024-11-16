593d651085e240de9cea055d2a845db23d3363bc
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-11-16 22:31:19 +0530

    React.Fragment Uderstood

--- react_demo/src/App.jsx
+++ react_demo/src/App.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+// import React from "react";
+import Fragment from "react";
 
 export const App = () => {
-  return React.createElement(
-    "div",
-    null,
-    React.createElement("h1", null, "Hello World")
+  return (
+    // <React.Fragment>
+    //   <h1>Welcome to My First React App!</h1>
+    //   <div className="card">
+    //     <p>Hello! I am learning React</p>
+    //     <button onClick={() => alert("You clicked the button!")}>
+    //       Click me!
+    //     </button>
+    //   </div>
+    // </React.Fragment>
+
+    <Fragment>
+      <h1>Welcome to My First React App!</h1>
+      <div className="card">
+        <p>Hello! I am learning React</p>
+        <button onClick={() => alert("You clicked the button!")}>
+          Click me!
+        </button>
+      </div>
+    </Fragment>
   );
 };
