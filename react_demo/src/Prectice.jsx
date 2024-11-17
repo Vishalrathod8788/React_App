44cf4116d66c0bbce3b4bf1956ee0c0856ef4cf8
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-11-17 17:12:42 +0530

    Some code add and many method to write code

--- react_demo/src/Prectice.jsx
+++ react_demo/src/Prectice.jsx
@@ -1,7 +1,25 @@
 export const Prectice = () => {
+  const Students = [1];
   return (
     <>
       <h1> Interview </h1>
+      {/* <p>{Students.length && "Students Not Found"}</p> */}
+      <p>Number Of Students : {Students.length}</p>
+      {/* Solution : React Doesn't render anything if the condition is false 
+      it mean our answer is come to 0 so React doesn't anything print */}
+      <p>
+        Number Of Students : {Students.length === 0 && "Students Not Found"}{" "}
+        {/* It mean this is return Falsy Value
+        so Dose not print anything */}
+      </p>
+
+      {/* Solution 1: */}
+      <p>
+        Number Of Students : {Students.length != 0 ? "Students Not Found" : 0}
+      </p>
+
+      {/* Solution 2: */}
+      <p>Number Of Students : {Students.length || 0}</p>
     </>
   );
 };
--- react_demo/src/main.jsx
+++ react_demo/src/main.jsx
@@ -1,12 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// import { NetFlix5 } from "./App";
+
 import { Prectice } from "./Prectice";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <NetFlix5 /> */}
     <Prectice />
   </StrictMode>
 );
