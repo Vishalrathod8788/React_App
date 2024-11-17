d6f165a6c4ac6489e3339632681e6f537678d889
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-11-17 09:16:47 +0530

    new page in create component

---
+++ react_demo/src/FindAge.jsx
@@ -0,0 +1,8 @@
+
+export const FindAge = () => {
+    return (
+        <div>
+            <h1>Find Age</h1>
+        </div>
+    )
+}
--- react_demo/src/main.jsx
+++ react_demo/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { MyComponent } from "./App.jsx";
+import { FindAge } from "./FindAge.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <MyComponent />
+    <FindAge />
   </StrictMode>
 );
