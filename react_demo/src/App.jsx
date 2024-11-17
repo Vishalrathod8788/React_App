e2ed56cf91be32e928aec54480618e9498a34e32
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-11-17 09:12:59 +0530

    Create New Component

--- react_demo/src/App.jsx
+++ react_demo/src/App.jsx
@@ -1,43 +1,49 @@
 // import React from "react";
-import { Fragment } from "react";
+// import { Fragment } from "react";
 
 export const App = () => {
+  // return (
+  // You Can't One more component return in JavaScript
+  // but You can Component value add in Array with Key and Pair to store and return
+  // but It's complicate so You will use to return <> write component code <> this is absulutly right
+  // Method 1 :
+  // <>
+  //   <h1>Welcome to My First React App!</h1>
+  //   <div className="card">
+  //     <p>Hello! I am learning React</p>
+  //     <button onClick={() => alert("You clicked the button!")}>
+  //       Click me!
+  //     </button>
+  //   </div>
+  // </>
+  // Method 2 :
+  // <React.Fragment>
+  //   <h1>Welcome to My First React App!</h1>
+  //   <div className="card">
+  //     <p>Hello! I am learning React</p>
+  //     <button onClick={() => alert("You clicked the button!")}>
+  //       Click me!
+  //     </button>
+  //   </div>
+  // </React.Fragment>
+  // Method 3 :
+  // <Fragment>
+  //   <h1>Welcome to My First React App!</h1>
+  //   <div className="card">
+  //     <p>Hello! I am learning React</p>
+  //     <button onClick={() => alert("You clicked the button!")}>
+  //       Click me!
+  //     </button>
+  //   </div>
+  // </Fragment>
+  // );
+};
+
+export const MyComponent = () => {
   return (
-    // You Can't One more component return in JavaScript
-    // but You can Component value add in Array with Key and Pair to store and return
-    // but It's complicate so You will use to return <> write component code <> this is absulutly right
-
-    // Method 1 :
-    // <>
-    //   <h1>Welcome to My First React App!</h1>
-    //   <div className="card">
-    //     <p>Hello! I am learning React</p>
-    //     <button onClick={() => alert("You clicked the button!")}>
-    //       Click me!
-    //     </button>
-    //   </div>
-    // </>
-
-    // Method 2 :
-    // <React.Fragment>
-    //   <h1>Welcome to My First React App!</h1>
-    //   <div className="card">
-    //     <p>Hello! I am learning React</p>
-    //     <button onClick={() => alert("You clicked the button!")}>
-    //       Click me!
-    //     </button>
-    //   </div>
-    // </React.Fragment>
-
-    // Method 3 :
-    <Fragment>
-      <h1>Welcome to My First React App!</h1>
-      <div className="card">
-        <p>Hello! I am learning React</p>
-        <button onClick={() => alert("You clicked the button!")}>
-          Click me!
-        </button>
-      </div>
-    </Fragment>
+    <>
+      <h1>My Component</h1>
+      <p>{1 + 2 + 3}</p>
+    </>
   );
 };
