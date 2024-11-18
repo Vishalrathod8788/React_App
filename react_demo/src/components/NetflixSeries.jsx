901852da0e338499a3062735b04f3e1fd0dfe634
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-11-18 18:29:08 +0530

    Many code write in many file

---
+++ react_demo/src/components/NetflixSeries.jsx
@@ -0,0 +1,31 @@
+export const NetFlix = () => {
+  const netflix = {
+    name: "Stranger Things",
+    rating: "8.7/10",
+    summary:
+      "When a young boy disappears, his mother, a police chief, and his friends must confront terrifying supernatural forces in order to get him back.",
+    genre: "Drama, Fantasy, Horror",
+    age: 18,
+  };
+  const canWatch = () => {
+    if (netflix.age >= 18) return "Watch Now";
+    return "Not Available";
+  };
+
+  return (
+    <>
+      <div>
+        <img
+          src="https://m.media-amazon.com/images/M/MV5BN2ZmYjg1YmItNWQ4OC00YWM0LWE0ZDktYThjOTZiZjhhN2Q2XkEyXkFqcGdeQXVyNjgxNTQ3Mjk@._V1_.jpg"
+          alt="Stranger Things"
+          style={{ width: "300px" }}
+        ></img>
+      </div>
+      <h2>Name : {netflix.name}</h2>
+      <h3>Rating : {netflix.rating}</h3>
+      <p>summary : {netflix.summary}</p>
+      <p>Genre : {netflix.genre}</p>
+      <button>{canWatch()}</button>
+    </>
+  );
+};
--- react_demo/src/main.jsx
+++ react_demo/src/main.jsx
@@ -2,11 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-// import { Prectice } from "./Prectice";
-import { NetFlix5 } from "./App";
+import { NetFlix } from "./components/NetflixSeries";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <NetFlix5 />
+    <NetFlix />
   </StrictMode>
 );
