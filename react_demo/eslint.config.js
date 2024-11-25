bdd8eaeded0397a2f2597a0422abb07ca6bb19c4
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-11-25 16:26:11 +0530

    some configuration add

--- react_demo/eslint.config.js
+++ react_demo/eslint.config.js
@@ -1,6 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
-import react from "eslint-plugin-react";
+import react, { rules } from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 
@@ -22,6 +22,9 @@ export default [
       react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+    },
+    rules: {
+      "react/prop-types": "off",
     },
 
     rules: {
