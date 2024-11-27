7492f4aa08c82ee93aaf0085d17dd5e0598e74d7
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-11-27 17:41:33 +0530

    profile.json file create

--- react_demo/src/components/ProfileCard.jsx
+++ react_demo/src/components/ProfileCard.jsx
@@ -1,6 +1,3 @@
-// Create new component profile card without using CSS ans Simple Information like Name, age, greeting, hobbies so on
-// File path: /home/vishal/React_App/react_demo/src/components/ProfileCard.jsx
-// Content:
 export const ProfileCard = ({ data }) => {
   const { name, age, greeting, hobbies } = data;
   return (
