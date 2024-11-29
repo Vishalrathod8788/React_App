254b7bdc21492c3297a771e1140b675374b76a7c
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-11-29 18:19:17 +0530

    Icard in Student data manualy add

---
+++ react_demo/src/components/Icard.jsx
@@ -0,0 +1,13 @@
+import studentData from "../api/studentData.json";
+export const Icard = () => {
+  return (
+    <>
+      <p>Roll No. : 24MCA30</p>
+      <p>First Name : Rathod</p>
+      <p>Last Name : Vishal</p>
+      <p>Age : 20</p>
+      <p>Grade : A+</p>
+      <p>IsPresent : Yes</p>
+    </>
+  );
+};
