e574319b1bf606ca552a3aa94e55303d73c466da
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-11-30 10:05:17 +0530

    Implement ContactCard component with hardcoded contact data and map to ContactList

--- react_demo/src/components/ContactCard.jsx
+++ react_demo/src/components/ContactCard.jsx
@@ -1,7 +1,28 @@
+import { ContacList } from "./ContactList";
 export const ContactCard = () => {
+  const contact = [
+    {
+      name: "vishal",
+      phone: "6353008705",
+      email: "coder@vishal.sbs",
+    },
+    {
+      name: "Alice",
+      phone: "1234567890",
+      email: "alice@gmail.com",
+    },
+    {
+      name: "Bob",
+      phone: "1234567890",
+      email: "XXXXXXXXXXXXXXXX",
+    },
+  ];
   return (
     <div>
       <h1>ContactCard</h1>
+      {contact.map((contact, index) => {
+        return <ContacList key={contact.index} prop={contact} />;
+      })}
     </div>
   );
 };
