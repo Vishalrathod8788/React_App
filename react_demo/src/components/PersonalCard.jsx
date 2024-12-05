35be6f5334858932787a207e547f974b1b79a4e9
Vishal Rathod <vishalrathod1562004@gmail.com>
2024-12-06 00:36:37 +0530

    Remove Netflix.module.css and refactor PersonalCard component to improve layout and styling; added CSS module for better design and readability.

--- react_demo/src/components/PersonalCard.jsx
+++ react_demo/src/components/PersonalCard.jsx
@@ -1,11 +1,13 @@
+import style from "./Profile.module.css";
+
 export const PersonalCard = ({ data }) => {
   const { name, age, occupation, location } = data;
   return (
-    <li>
-      <p>Name : {name}</p>
-      <p>Age : {age}</p>
-      <p>Occupation : {occupation}</p>
-      <p>Location : {location}</p>
+    <li className={style.card}>
+      <h1 style={{ color: "grey" }}>Name : {name}</h1>
+      <h3>Age : {age}</h3>
+      <p >Occupation : {occupation}</p>
+      <h3>Location : {location}</h3>
     </li>
   );
 };
