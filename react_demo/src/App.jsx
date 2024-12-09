072c6f73b2841aa2f0accca1a6746cc654f69850
Vishal Rathod <vishalrathod1562004@gmail.com>
2024-12-09 13:52:46 +0530

    Refactor App component to integrate NetflixSeries; commented out EventHandling component for future use. Update SeriesCard to use styled-components for improved styling and consistency, and replace CSS module references. Clean up style.css by removing unnecessary whitespace.

--- react_demo/src/App.jsx
+++ react_demo/src/App.jsx
@@ -1,10 +1,12 @@
 // import { ProfilesCard } from "./components/ProfileCard";
 import "./components/style.css";
-import { EventHandling } from "./components/EventHandling";
+// import { EventHandling } from "./components/EventHandling";
+import { NetflixSeries } from "./components/NetflixSeries";
 export const App = () => {
   return (
     <>
-      <EventHandling />
+      {/* <EventHandling /> */}
+      <NetflixSeries />
     </>
   );
 };
--- react_demo/src/components/SeriesCard.jsx
+++ react_demo/src/components/SeriesCard.jsx
@@ -1,20 +1,10 @@
+import myStyle from "./Netflix.module.css";
+
+import styled from "styled-components";
+
 import "./style.css";
 export const SeriesCard = ({ data }) => {
-  //   console.log(props);
   const { img_url, name, rating, description, cast, genre, watch_url } = data;
-
-  // const btn_style = { }
-  // const ButtonThapa = styled.button({
-  //   padding: "1.2rem 2.4rem",
-  //   border: "none",
-  //   fontSize: "1.6rem",
-  //   backgroundColor: `${rating >= 8.5 ? "#7dcea0" : "#f7dc6f"}`,
-  //   color: "var(--btn-color)",
-  //   fontWeight: "bold",
-  //   cursor: "pointer",
-  // });
-
-  // const btn_style = { }
   const ButtonThapa = styled.button`
     padding: 1.2rem 2.4rem;
     border: none;
@@ -24,6 +14,7 @@ export const SeriesCard = ({ data }) => {
     color: var(--btn-color);
     font-weight: bold;
     cursor: pointer;
+    radius: 5px;
   `;
 
   //  In styled-components, you can pass a function within the template literal to dynamically set CSS properties based on props or state.
@@ -34,23 +25,21 @@ export const SeriesCard = ({ data }) => {
     text-transform: capitalize;
   `;
 
-  const ratingClass = rating >= 8.5 ? styles.super_hit : styles.average;
+  const ratingClass = rating >= 8.5 ? myStyle.super_hit : myStyle.average;
 
   return (
-    <li className={styles.card}>
+    <li className={myStyle.card}>
       <div>
-        <img src={img_url} alt={name} width="40%" height="40%" />
+        <img src={img_url} alt={name} />
       </div>
-      {/* <div className={styles["card-content"]}> */}
+      {/* <div className={myStyle["card-content"]}> */}
       <div className="flex flex-col gap-6 py-[3.2rem] px-[1.2rem]">
         <h2>Name: {name} </h2>
         <Rating>
           Rating:
-          <span className={`${styles.rating} ${ratingClass}`}>{rating}</span>
+          <span className={`${myStyle.rating} ${ratingClass}`}>{rating}</span>
         </Rating>
-        <p>
-          Summary: {description}{" "}
-        </p>
+        <p>Summary: {description} </p>
         <p>Genre: {genre.join(", ")} </p>
         <p>Cast: {cast.join(", ")}</p>
         <a href={watch_url} target="_blank">
