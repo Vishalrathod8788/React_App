b1205c6f82c9b71bc1d8fcf262ef7234a85f2b50
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-01-05 13:17:33 +0530

    Refactor EventHandling component to remove unnecessary console logs in handleClick function; update button click handling for improved event passing.

--- react_demo/src/components/EventHandling.jsx
+++ react_demo/src/components/EventHandling.jsx
@@ -2,11 +2,10 @@ import "./EV.css";
 
 export const EventHandling = () => {
   const handleClick = (event) => {
-    // console.log(event);
-    // console.log(event.target);
-    // console.log(event.type);
-    // console.log(event.target.textContent);
-    // console.log(event.view.document.getElementById("root"));
+    console.log(event);
+    console.log(event.target);
+    console.log(event.type);
+    console.log(event.target.textContent);
     alert("Button Clicked! ");
   };
 
@@ -17,7 +16,7 @@ export const EventHandling = () => {
       <button onClick={handleClick}>Click Me!</button>
 
       {/* that mean first user click on text then function call handlClick function and this function return the value */}
-      <button onClick={() => handleClick()}>Click Me 2</button>
+      <button onClick={(event) => handleClick(event)}>Click Me 2</button>
     </>
   );
 };
