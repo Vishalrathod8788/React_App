4d3c6c447ee4199f801e1a3d0157d61c4ce399e3
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-02-04 17:14:11 +0530

    Refactor State component; implement useState for count management and update button label to 'Increment'.

--- react_demo/src/components/hooks/States.jsx
+++ react_demo/src/components/hooks/States.jsx
@@ -1,18 +1,24 @@
+import { useState } from "react";
+
 export const State = () => {
-  let count = 0;
-  
+  const handleClickButton = () => {
+    setCount(() => count + 1);
+  };
+
+  let [count, setCount] = useState(0);
   return (
     <>
       <section
         className="main-div"
         style={{
-          textAlign: "center",
+          // textAlign: "center",
           flexDirection: "column",
           fontSize: "4rem",
+          paddingLeft: "27rem",
         }}
       >
         <h1>{count}</h1>
-        <button onClick={handleClickButton}>Click Me!</button>
+        <button onClick={handleClickButton}>Increment</button>
       </section>
     </>
   );
