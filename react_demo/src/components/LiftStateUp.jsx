b2996758f2bfef1fa95e2dbc923aa19afa03160e
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-02-05 18:02:40 +0530

    Refactor LiftStateUp component; enhance input handling and display structure

--- react_demo/src/components/LiftStateUp.jsx
+++ react_demo/src/components/LiftStateUp.jsx
@@ -1,30 +1,35 @@
-export const ListStateUp = () => {
-  const [Input, setInput] = useState("My name is John Doe");
+import { useState } from "react";
+
+export const LiftStateUp = () => {
+  const [Input, setInput] = useState("");
   return (
     <>
-      <InputComponent />
-      <DisplayComponent />
+      <InputComponent Input={Input} setInput={setInput} />
+      <DisplayComponent Input={Input} />
     </>
   );
+};
 
-  const InputComponent = () => {
-    return (
-      <>
-        <input
-          type="text"
-          value={Input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button onClick={() => setInput("")}>Clear</button>
-      </>
-    );
-  };
+const InputComponent = ({ Input, setInput }) => {
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Enter your text here"
+        value={Input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={() => setInput("")}>Clear</button>
+    </>
+  );
+};
 
-  const DisplayComponent = () => {
-    return (
-      <>
+const DisplayComponent = ({ Input }) => {
+  return (
+    <>
+      <div className="div-main">
         <h1>{Input}</h1>
-      </>
-    );
-  };
+      </div>
+    </>
+  );
 };
