a9f02a061be16baa5708118588dcecc57d026653
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-02-08 00:05:45 +0530

    Enhance Todo component with date and time display, improve task rendering, and add check/delete buttons

--- react_demo/src/Project/Todo/Todo.jsx
+++ react_demo/src/Project/Todo/Todo.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import "./Todo.css";
+import { MdCheck, MdDeleteForever } from "react-icons/md";
 
 export const Todo = () => {
   const [inputValue, setInputValue] = useState("");
   const [tasks, setTasks] = useState([]);
+  const [dateTime, setDateTime] = useState("");
 
   const handleInputChange = (inputValue) => {
     setInputValue(inputValue);
@@ -12,51 +14,73 @@ export const Todo = () => {
   const handleFormSubmit = (event) => {
     event.preventDefault();
 
-    if (!inputValue) return alert("please write some task");
-
-    setTasks([...tasks, inputValue]);
+    if (!inputValue) return;
 
     if (tasks.includes(inputValue)) {
-      //   alert("Task already exists");
+      alert("Task already exists");
       setInputValue("");
       return;
     }
+
+    setTasks([...tasks, inputValue]);
 
     if (inputValue) {
       setInputValue("");
     }
   };
 
+  //Todo - Add date and time
+
+  setInterval(() => {
+    const date = new Date();
+    const formatedDate = date.toLocaleDateString();
+    const formatedTime = date.toLocaleTimeString();
+
+    setDateTime(`${formatedDate} - ${formatedTime}`);
+  }, 1000);
+
   return (
     <section className="todo-container">
       <header>
         <h1>Todo List</h1>
-
-        <section className="form">
-          <form onSubmit={handleFormSubmit}>
-            <div>
-              <input
-                type="text"
-                placeholder="Add a new task"
-                className="todo-input"
-                autoComplete="false"
-                value={inputValue}
-                onChange={(event) => handleInputChange(event.target.value)}
-              />
-            </div>
-            <button type="submit" className="todo-btn">
-              Add task
-            </button>
-          </form>
-        </section>
-        <section className="myUnOrdList">
-          <ul>
-          task.map((curTask, index) => {
-            return <li key={index}> {curTask} </li>;
-          });
-          </ul>
-        </section>
+        <h2 className="date-time">{dateTime}</h2>
       </header>
+      <section className="form">
+        <form onSubmit={handleFormSubmit}>
+          <div>
+            <input
+              type="text"
+              placeholder="Add a new task"
+              className="todo-input"
+              autoComplete="false"
+              value={inputValue}
+              onChange={(event) => handleInputChange(event.target.value)}
+            />
+          </div>
+          <button type="submit" className="todo-btn">
+            Add task
+          </button>
+        </form>
+      </section>
+      <section className="myUnOrdList">
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index} className="todo-item">
+              <span>{task}</span>
+              <button
+                style={{
+                  backgroundColor: "#f2f3f4",
+                }}
+              >
+                <MdCheck className="check-btn" />
+              </button>
+              <button style={{ backgroundColor: "#f2f3f4" }}>
+                <MdDeleteForever className="delete-btn" />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </section>
     </section>
   );
 };
