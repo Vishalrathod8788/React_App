9217cd92e5286d94a77e5428044f91d072513004
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-02-07 12:54:27 +0530

    Implement ToggleSwitch functionality; add click handler and display state label

--- react_demo/src/Project/ToggleSwitch/ToggleSwitch.jsx
+++ react_demo/src/Project/ToggleSwitch/ToggleSwitch.jsx
@@ -3,12 +3,14 @@ import "./ToggleSwitch.css";
 
 export const ToggleSwitch = () => {
   const [isOn, setIsOn] = useState(false);
+
+  const handleOnOff = () => {}
   return (
     <div className="toggle-container">
-      <div className={`large-toggle-switch`}>
-        <div className="switch-handle"></div>
+      <div className={`large-toggle-switch`} onClick={handleOnOff}>
+        <div className={`switch-handle ${is}`}></div>
       </div>
-      <p className="switch-label"></p>
+      <p className="switch-label">{isOn ? "ON" : "OFF"}</p>
     </div>
   );
 };
