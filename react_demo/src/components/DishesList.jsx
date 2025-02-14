8447f79a2ffc4fca3da1aea267ebc0cf0d127b4d
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-02-14 18:15:27 +0530

    Remove unused components and styles from the project

--- react_demo/src/components/DishesList.jsx
+++
@@ -1,8 +0,0 @@
-export const DishesList = ({ name, price }) => {
-  return (
-    <div style={{ border: "1px solid gray", margin: "10px", padding: "10px" }}>
-      <h3>Name : {name}</h3>
-      <h3>Price : {price}</h3>
-    </div>
-  );
-};
